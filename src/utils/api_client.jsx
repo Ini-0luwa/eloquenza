80c21efe0dbc9e306e93d134118006ffd308bf08
oluwafemi <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>
2022-10-29 15:46:00 +0100

    eloskincare

--- src/utils/api_client.jsx
+++ src/utils/api_client.jsx
@@ -3,6 +3,7 @@
 // require('dotenv').config()
 let API ='https://naijarestaurants.herokuapp.com'
 // console.log(API);
+let slug = 'eloquenza'
 
 export const API_ROUTES = {
   REGISTER : `${API}/account/register`,
@@ -17,9 +18,24 @@ export const API_NAVIGATE = {
 }
 
 
+
 export const STORE_ENDPOINT ={
   STORE: `${API}/store`,
-  STORE_ID: `${API}/store/eloquenza`,
+  STORE_ID: `${API}/store/${slug}`,
+  STORE_ID_CART: `${API}/store/${slug}/cart`,
+  STORE_ID_CART_POST: `${API}/store/${slug}/cart`,
+  STORE_ID_CATEGORY: `${API}/store/${slug}/categories`,
+  STORE_ID_CATEGORY_ID: `${API}/store/${slug}/categories/${slug}`,
+  STORE_ID_CHECKOUT: `${API}/store/eloquenza/checkout`,
+  STORE_ID_CHECKOUT_POST: `${API}/store/eloquenza/checkout`,
+  STORE_ID_CHECKOUT_PAYMENT_POST: `${API}/store/eloquenza/checkout-payment`,
+  STORE_ID_DETAILS: `${API}/store/eloquenza/details`,
+  STORE_ID_PRODUCTS: `${API}/store/eloquenza/products`,
+  STORE_ID_PRODUCTS_ID: `${API}/store/eloquenza/products/eloquenza`,
+  STORE_ALL: `${API}/store/all`,
+  STORE_CARD_PAYMENT: `${API}/store/complete-card-payment`,
+  STORE_ORDER: `${API}/store/order`,
+  STORE_ORDER_ID: `${API}/store/order/eloquenza`,
 }
 
 // console.log(process.env.NODE_ENV)
